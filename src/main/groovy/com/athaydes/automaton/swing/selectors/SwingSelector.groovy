95f6a6c66bf23c55d805468275c163c2afadb7ae
renatoathaydes <renatoathaydes@hotmail.com>
2015-09-27 18:47:24 +0200

    Created SwingSelector

---
+++ src/main/groovy/com/athaydes/automaton/swing/selectors/SwingSelector.groovy
@@ -0,0 +1,134 @@
+package com.athaydes.automaton.swing.selectors
+
+import groovy.transform.Canonical
+import groovy.transform.CompileStatic
+
+import javax.swing.table.TableColumn
+import javax.swing.tree.TreeNode
+
+import static com.athaydes.automaton.ReflectionHelper.callMethodIfExists
+
+/**
+ * A Swing component selector.
+ *
+ * There are methods for selecting a single element (<code>selectWithX</code>)
+ * and for selecting all elements (<code>selectAllWithX</code> matching a certain predicate.
+ *
+ * If a method to select a single element is used and the element is not found, the return value will be null.
+ */
+@CompileStatic
+@Canonical
+class SwingSelector {
+
+    /**
+     * The root element to search from
+     */
+    def root = null
+
+    /**
+     * Select an element with the given type.
+     *
+     * Notice that instances of subtypes of the given type are also selected.
+     * @param type to select
+     * @return instance of the given type if any, null otherwise.
+     */
+    def <T> T selectWithType( Class<T> type ) {
+        firstOrNull selectAllWithType( type, 1 )
+    }
+
+    /**
+     * Select an element with the given text.
+     *
+     * The text of an element is usually what is seen on the screen, not necessarily what its value may be.
+     * @param text of element to be selected
+     * @return element with the given text if any, null otherwise.
+     */
+    def selectWithText( String text ) {
+        firstOrNull selectAllWithText( text, 1 )
+    }
+
+    /**
+     * Select an element with the given name.
+     *
+     * @param name of element to be selected
+     * @return element with the given name if any, null otherwise.
+     */
+    def selectWithName( String name ) {
+        firstOrNull selectAllWithName( name, 1 )
+    }
+
+    /**
+     * Select all elements with the given type.
+     *
+     * Notice that instances of subtypes of the given type are also selected.
+     * @param type to select
+     * @param limit maximum number of elements to return
+     * @return all instances of the given type.
+     */
+    def <T> List<T> selectAllWithType( Class<T> type, int limit = Integer.MAX_VALUE ) {
+        selectItems( limit ) { item -> type.isInstance( item ) } as List<T>
+    }
+
+    /**
+     * Select all elements with the given text.
+     *
+     * The text of an element is usually what is seen on the screen, not necessarily what its value may be.
+     * @param text of elements to be selected
+     * @param limit maximum number of elements to return
+     * @return all elements with the given text.
+     */
+    def List selectAllWithText( String text, int limit = Integer.MAX_VALUE ) {
+        selectItems( limit ) { item ->
+            switch ( item ) {
+                case TableColumn: return ( item as TableColumn ).headerValue == text
+                case TreeNode: return ( item as TreeNode ).toString() == text
+                default: return callMethodIfExists( item, 'getText' ) == text
+            }
+        }
+    }
+
+    /**
+     * Select all elements with the given name.
+     *
+     * @param name of elements to be selected
+     * @param limit maximum number of elements to return
+     * @return all elements with the given name.
+     */
+    def List selectAllWithName( String name, int limit = Integer.MAX_VALUE ) {
+        selectItems( limit ) { item ->
+            callMethodIfExists( item, 'getName' ) == name
+        }
+    }
+
+    /**
+     * Selects all Swing items for which the given visitor returns Groovy truth.
+     * @param limit maximum number of elements to return
+     * @param visitor a closure which takes a Swing item as argument, returning an Object satisfying Groovy truth
+     * if the Swing item is to be included in the result.
+     * @return List of all Swing items that have been selected.
+     */
+    List selectItems( int limit, Closure visitor ) {
+        List result = [ ]
+        if ( limit < 1 ) {
+            return result
+        }
+        SwingNavigator.navigateBreadthFirst( root ) { component ->
+            if ( visitor.call( component ) ) {
+                result << component
+                return result.size() >= limit
+            }
+            return false
+        }
+        return result
+    }
+
+    /**
+     * Returns the first element of the List if it is not empty, null otherwise.
+     * @param list of elements
+     * @return first element of the List or null.
+     */
+    static firstOrNull( List list ) {
+        list.empty ? null : list.first()
+    }
+
+}
---
+++ src/test/groovy/com/athaydes/automaton/swing/selectors/SwingSelectorTest.groovy
@@ -0,0 +1,153 @@
+package com.athaydes.automaton.swing.selectors
+
+import groovy.swing.SwingBuilder
+import spock.lang.Specification
+import spock.lang.Subject
+
+import javax.swing.JDialog
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.JTextArea
+import javax.swing.JTextField
+import javax.swing.table.TableCellRenderer
+import javax.swing.table.TableColumn
+import javax.swing.tree.TreeNode
+import java.awt.Dimension
+import java.awt.Point
+import java.awt.Window
+
+class SwingSelectorTest extends Specification {
+
+    JFrame jframe = new SwingBuilder().frame( show: false, name: 'FRAME' ) {
+        def tModel = [ [ firstCol: 'item 1 - Col 1', secCol: 'item 1 - Col 2' ] ]
+        dialog( show: false, name: 'The dialog' ) {
+            vbox {
+                label( 'This is a dialog' )
+                textField( 'write something...' )
+            }
+        }
+        scrollPane {
+            table {
+                tableModel( list: tModel ) {
+                    propertyColumn( header: 'Col 1', propertyName: 'firstCol' )
+                    propertyColumn( header: 'Col 2', propertyName: 'secCol' )
+                }
+            }
+        }
+        scrollPane {
+            tree( rootVisible: false )
+        }
+
+        window( show: false, name: 'some window',
+                location: [ 350, 250 ] as Point,
+                size: [ 150, 250 ] as Dimension ) {
+            vbox {
+                label( 'A window', name: 'label-in-a-window' )
+                textArea( 'Write text here.' )
+                textArea( 'write something...' )
+            }
+        }
+    }
+
+    @Subject
+    def selector = new SwingSelector( root: jframe )
+
+    @SuppressWarnings( "GroovyAssignabilityCheck" )
+    def "Can select an element by type"() {
+        given: 'The large UI used by this test'
+
+        when: 'Trying to select an existing component by type'
+        def result = selector.selectWithType( type )
+
+        then: 'The selected component meets the expected conditions'
+        type.isInstance( result )
+        assertion.call( result )
+
+        where:
+        type      | assertion
+        JFrame    | { JFrame frame -> frame?.name == 'FRAME' }
+        JDialog   | { JDialog dialog -> dialog?.name == 'The dialog' }
+        JTextArea | { JTextArea ta -> ta?.text == 'Write text here.' }
+        TreeNode  | { TreeNode tn -> tn?.toString() == 'JTree' }
+    }
+
+    def "Can select all elements by type"() {
+        given: 'The large UI used by this test'
+
+        when: 'Trying to select existing components by type'
+        def result = selector.selectAllWithType( type )
+
+        then: 'The selected component meets the expected conditions'
+        result.every { type.isInstance( it ) }
+        assertion.call( result )
+
+        where:
+        type              | assertion
+        JFrame            | { List frames -> frames?.size() == 1 }
+        JDialog           | { List dialogs -> dialogs?.size() == 1 }
+        JTextArea         | { List tas -> tas?.size() == 2 }
+        TableCellRenderer | { List tcrs -> tcrs?.size() == 2 }
+    }
+
+    def "Can select all elements by name"() {
+        given: 'The large UI used by this test'
+
+        when: 'Trying to select existing components by name'
+        def result = selector.selectAllWithName( name )
+
+        then: 'The selected component meets the expected conditions'
+        assertion.call( result )
+
+        where:
+        name                | assertion
+        'FRAME'             | { List list -> list?.size() == 1 && list.first() instanceof JFrame }
+        'The dialog'        | { List list -> list?.size() == 1 && list.first() instanceof JDialog }
+        'some window'       | { List list -> list?.size() == 1 && list.first() instanceof Window }
+        'label-in-a-window' | { List list -> list?.size() == 1 && list.first() instanceof JLabel }
+    }
+
+    def "Can select all elements by text"() {
+        given: 'The large UI used by this test'
+
+        when: 'Trying to select existing components by text'
+        def result = selector.selectAllWithText( text )
+
+        then: 'The selected component meets the expected conditions'
+        assertion.call( result )
+
+        where:
+        text                 | assertion
+        'Col 1'              | { List list -> list?.size() == 1 && list.first() instanceof TableColumn }
+        'item 1 - Col 2'     | { List list -> list?.size() == 1 && list.first() instanceof JLabel }
+        'red'                | { List list -> list?.size() == 1 && list.first() instanceof TreeNode }
+        'A window'           | { List list -> list?.size() == 1 && list.first() instanceof JLabel }
+        'write something...' | { List list -> list?.size() == 2 && list.any { it instanceof JTextField } }
+        'write something...' | { List list -> list?.size() == 2 && list.any { it instanceof JTextArea } }
+    }
+
+    def "Stops navigating as soon as the visitor returns true"() {
+        given: 'The large UI used by this test'
+
+        when: 'Selecting all items up to an instance of JFrame being met'
+        def result = selector.selectItems( 100 ) { item ->
+            item instanceof JFrame
+        }
+
+        then: 'All items visited up to the JFrame are returned'
+        result.size() == 1
+        result.first() instanceof JFrame
+    }
+
+    def "Stops navigating as soon as the limit is reached"() {
+        given: 'The large UI used by this test'
+
+        when: 'Selecting up to a certain number of items unconditionally'
+        def result = selector.selectItems( limit ) { true }
+
+        then: 'Only the limit number of items should have been visited'
+        result.size() == limit
+
+        where:
+        limit << [ 0, 1, 2, 10 ]
+    }
+}
